81fcdaaca9c06132630e71f917266fd09988f071
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2024-03-03 10:21:49 +0530

    set apis
    crus operations

--- controller.js
+++ controller.js
@@ -1,23 +1,59 @@
-const users = [
-    { id: 1, name: "Sumedha" },
-    { id: 2, name: "Sankalpa" },
-  ];
-  
-  
-  //======= get-all-users ==========
-  const getUsers =(cb) => {
-      cb(users)
-  };
-  
-  
-  //======= get-user-by-id ==========
-  const getUserById = (id,cb)=> {
-      const user = users.find(user => user.id == id)
-      cb(user)
-  };
-  
-  
-  //====exports-controllers========
-  exports.getUsers = getUsers;
-  exports.getUserById = getUserById;
-  
+const { response } = require("./app");
+const User = require("./model");
+
+//======= get-users-from-mongoDB ==========
+const getUsers = (req, res, next) => {
+  User.find()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error: error });
+    });
+};
+
+//=====Create-User=======
+const addUser = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name,
+  });
+  user
+    .save()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error: error });
+    });
+};
+
+//======Edit-User=============
+const updateUser = (req, res, next) => {
+  const { id, name } = req.body;
+  User.updateOne({ id: id }, { $set: { name: name } })
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error: error });
+    });
+};
+
+//=========Delete-User==========
+const deleteUser = (req, res, next) => {
+const id = req.body.id;
+User.deleteOne({ id: id })
+  .then((response) => {
+    res.json({ response });
+  })
+  .catch((error) => {
+    res.json({ error: error });
+  });
+}
+
+//====exports-controllers========
+exports.getUsers = getUsers;
+exports.addUser = addUser;
+exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;
